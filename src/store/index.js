b372265b5f034f90cc163df3777c14690a832e04
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2022-08-17 12:45:58 +0600

    vue x install

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from 'vuex'
+
+// Create a new store instance.
+const store = createStore({
+  state :{
+   
+  },
+  getters:{
+
+  },
+  mutations: {
+  
+  },
+  actions:{
+
+  }
+});
+
+export default store;
